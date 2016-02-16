dbd50d4285ae57ba8c513c63a6527e3995ba3e5a
Randal Budnikas <budnikrp@jmu.edu>
2016-02-16 16:27:17 -0500

    Adding files

---
+++ My First App/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by b on 2/16/16.
+//  Copyright © 2016 b. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func example(){
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
